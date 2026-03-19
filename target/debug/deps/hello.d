43fc7c284f4c6fb7928cf8c25c3e8722420e73a2
agemtes2 <agemtes2@students.towson.edu>
2026-03-19 19:35:01 -0400

    first commit

---
+++ target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\antik\hello\target\debug\deps\hello.d: src\main.rs
+
+C:\Users\antik\hello\target\debug\deps\hello.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\antik\hello\target\debug\hello.exe: C:\Users\antik\hello\src\main.rs
